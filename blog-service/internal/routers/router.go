c4eddb0c3fe2a3d8a4b6483df98bb19dce3fd01e
chenzhaoying <13160549408@163.com>
2020-09-26 20:33:45 +0800

    文件上传 全部完成

--- blog-service/internal/routers/router.go
+++ blog-service/internal/routers/router.go
@@ -3,10 +3,13 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-programming-tour-book/blog-service/docs"
+	"github.com/go-programming-tour-book/blog-service/global"
 	"github.com/go-programming-tour-book/blog-service/internal/middleware"
+	"github.com/go-programming-tour-book/blog-service/internal/routers/api"
 	v1 "github.com/go-programming-tour-book/blog-service/internal/routers/api/v1"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
 )
 
 func NewRouter() *gin.Engine {
@@ -18,6 +21,10 @@ func NewRouter() *gin.Engine {
 	article := v1.NewArticle()
 	tag := v1.NewTag()
 
+	upload := api.NewUpload()
+	r.POST("/upload/file", upload.UploadFile)
+	//在gin中实现File Server
+	r.StaticFS("/static", http.Dir(global.AppSetting.UploadSavePath))
 	apiv1 := r.Group("/api/v1")
 	{
 		//暂时不需要获取一个标签的需求
